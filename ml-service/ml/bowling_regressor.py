9d02a70328a0d280d86c4e0f5843407c1c4bfe99
umayangag <umayangag@gmail.com>
2025-11-02 22:58:38 +0100

    realign logic with prototype

--- ml-service/ml/bowling_regressor.py
+++ ml-service/ml/bowling_regressor.py
@@ -1,101 +1,25 @@
-import os
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import os
+import joblib
 from dataset_definitions import *
-from imblearn.over_sampling import SMOTE
-from sklearn import metrics, preprocessing, svm
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-
-RF = RandomForestClassifier(n_estimators=100)
-gnb = GaussianNB()
-clf = MLPClassifier(
-    solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(10, 5), random_state=1, max_iter=5000
-)
-SVM = svm.SVC(kernel="linear", C=1)
-regr = RandomForestRegressor(max_depth=100, random_state=0)
-reg = LinearRegression()
-mltreg = MultiOutputRegressor(regr)
-predictor = mltreg
 
 dirname = os.path.dirname(__file__)
-dataset_source = os.path.join(dirname, "output\\bowling_encoded.csv")
+output_dir = os.path.join(dirname, "../../output/ml-service")
 
-input_data = pd.read_csv(dataset_source)
-training_input_columns = input_bowling_columns.copy()
-training_input_columns.remove("player_name")
-
-X = input_data[training_input_columns]
-y = input_data[output_bowling_columns]  # Labels
-
-input_scaler = preprocessing.StandardScaler().fit(X)
-input_data_scaled = input_scaler.transform(X)
-X = pd.DataFrame(data=input_data_scaled, columns=X.columns)
-
-output_scaler = preprocessing.StandardScaler().fit(y)
-output_data_scaled = output_scaler.transform(y)
-y = pd.DataFrame(data=output_data_scaled, columns=y.columns)
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-train_set = 1465
-X_train = X.iloc[:train_set, :]
-X_test = X.iloc[train_set + 1 :, :]
-y_train = y.iloc[:train_set]
-y_test = y.iloc[train_set + 1 :]
-predictor.fit(X_train, y_train)
-
-predictor.fit(X_train, y_train)
-
+predictor = joblib.load(os.path.join(output_dir, "bowling_model.joblib"))
+input_scaler = joblib.load(os.path.join(output_dir, "bowling_scaler.joblib"))
+output_scaler = joblib.load(os.path.join(output_dir, "bowling_output_scaler.joblib"))
 
 def calculate_econ(row):
     if row["deliveries"] == 0:
         return 0
     return row["runs_conceded"] * 6 / row["deliveries"]
 
-
 def predict_bowling(dataset):
     scaled_dataset = input_scaler.transform(dataset)
     predicted = predictor.predict(scaled_dataset)
-    result = pd.DataFrame(
-        output_scaler.inverse_transform(predicted), columns=output_bowling_columns
-    )
+    result = pd.DataFrame(output_scaler.inverse_transform(predicted), columns=output_bowling_columns)
     for column in y.columns:
         dataset[column] = result[column]
     dataset["econ"] = dataset.apply(lambda row: calculate_econ(row), axis=1)
     return dataset
-
-
-def bowling_predict_test():
-    y_pred = predictor.predict(X_test)
-
-    comparison = {}
-    comparison["actual"] = y_test.to_numpy()
-    comparison["predicted"] = y_pred
-
-    for i in range(0, len(y_pred)):
-        print(
-            comparison["actual"][i],
-            " ",
-            comparison["predicted"][i],
-            "",
-        )
-
-    # print("Error:", metrics.mean_absolute_error(y_test, y_pred))
-    #
-    # print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-    # print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-    # print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-    # print('R2:', metrics.r2_score(y_test, y_pred))
-
-    print("Cross Validation Score:", cross_val_score(predictor, X, y, cv=10).max())
-
-
-if __name__ == "__main__":
-    bowling_predict_test()
--- ml-service/ml/calculate_features.py
+++ ml-service/ml/calculate_features.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from db import get_db_connection
+import pandas as pd
 
 
 def calculate_features(db_connection):
@@ -10,13 +10,17 @@ def calculate_features(db_connection):
     season_list = db_cursor.fetchall()
     db_cursor.execute("SELECT id, code FROM match_format")
     format_list = db_cursor.fetchall()
+    db_cursor.execute("SELECT id, venue_name FROM venue")
+    venue_list = db_cursor.fetchall()
+    db_cursor.execute("SELECT id, opposition_name FROM opposition")
+    opposition_list = db_cursor.fetchall()
 
     for season in season_list:
         for format in format_list:
             for player in players_list:
                 # Batting form
                 db_cursor.execute(
-                    f"SELECT bd.description, bd.runs, bd.balls, bd.minutes, bd.fours, bd.sixes, bd.strike_rate, md.season_id, md.format_id FROM batting_data bd LEFT JOIN match_details md ON bd.match_id = md.match_id WHERE bd.player_id = {player[0]} AND md.season_id = {season[0]} AND md.format_id = {format[0]}"
+                    f'SELECT bd.description, bd.runs, bd.balls, bd.minutes, bd.fours, bd.sixes, bd.strike_rate, md.season_id, md.format_id FROM batting_data bd LEFT JOIN match_details md ON bd.match_id = md.match_id WHERE bd.player_id = {player[0]} AND md.season_id = {season[0]} AND md.format_id = {format[0]}'
                 )
                 player_data = db_cursor.fetchall()
                 inning_count = len(player_data)
@@ -34,24 +38,12 @@ def calculate_features(db_connection):
                     fifties = len(df[df[1] >= 50]) - centuries
                     zeros = len(df[df[1] == 0])
 
-                    form = (
-                        0.4262 * average_score
-                        + 0.2566 * inning_count
-                        + 0.1510 * average_strike_rate
-                        + 0.0787 * centuries
-                        + 0.0556 * fifties
-                        - 0.0328 * zeros
-                    )
-                    print(
-                        f"Batting form for {player[1]} in season {season[1]} and format {format[1]}: {form}"
-                    )
-                    db_cursor.execute(
-                        f"INSERT INTO player_form_data_fmt (player_id, season_id, format_id, batting_form, bowling_form) VALUES ({player[0]}, {season[0]}, {format[0]}, {form}, 0) ON CONFLICT (player_id, season_id, format_id) DO UPDATE SET batting_form = {form}"
-                    )
+                    form = 0.4262 * average_score + 0.2566 * inning_count + 0.1510 * average_strike_rate + 0.0787 * centuries + 0.0556 * fifties - 0.0328 * zeros
+                    db_cursor.execute(f'INSERT INTO player_form_data_fmt (player_id, season_id, format_id, batting_form, bowling_form) VALUES ({player[0]}, {season[0]}, {format[0]}, {form}, 0) ON CONFLICT (player_id, season_id, format_id) DO UPDATE SET batting_form = {form}')
 
                 # Bowling form
                 db_cursor.execute(
-                    f"SELECT bw.balls, bw.runs, bw.wickets, bw.econ, md.season_id, md.format_id FROM bowling_data bw LEFT JOIN match_details md ON bw.match_id = md.match_id WHERE bw.player_id = {player[0]} AND md.season_id = {season[0]} AND md.format_id = {format[0]}"
+                    f'SELECT bw.balls, bw.runs, bw.wickets, bw.econ, md.season_id, md.format_id FROM bowling_data bw LEFT JOIN match_details md ON bw.match_id = md.match_id WHERE bw.player_id = {player[0]} AND md.season_id = {season[0]} AND md.format_id = {format[0]}'
                 )
                 player_data = db_cursor.fetchall()
                 inning_count = len(player_data)
@@ -62,25 +54,97 @@ def calculate_features(db_connection):
                     average = df[1].sum() / df[2].sum()
                     ff = len(df[df[2] >= 5])
 
-                    form = (
-                        0.4174 * total_overs
-                        + 0.2634 * inning_count
-                        + 0.1602 * strike_rate
-                        + 0.0975 * average
-                        + 0.0615 * ff
-                    )
-                    print(
-                        f"Bowling form for {player[1]} in season {season[1]} and format {format[1]}: {form}"
-                    )
-                    db_cursor.execute(
-                        f"UPDATE player_form_data_fmt SET bowling_form = {form} WHERE player_id = {player[0]} AND season_id = {season[0]} AND format_id = {format[0]}"
-                    )
+                    form = 0.4174 * total_overs + 0.2634 * inning_count + 0.1602 * strike_rate + 0.0975 * average + 0.0615 * ff
+                    db_cursor.execute(f'UPDATE player_form_data_fmt SET bowling_form = {form} WHERE player_id = {player[0]} AND season_id = {season[0]} AND format_id = {format[0]}')
+
+    for venue in venue_list:
+        for format in format_list:
+            for player in players_list:
+                # Batting venue
+                db_cursor.execute(
+                    f'SELECT bd.description, bd.runs, bd.balls, bd.minutes, bd.fours, bd.sixes, bd.strike_rate, md.venue_id, md.format_id FROM batting_data bd LEFT JOIN match_details md ON bd.match_id = md.match_id WHERE bd.player_id = {player[0]} AND md.venue_id = {venue[0]} AND md.format_id = {format[0]}'
+                )
+                player_data = db_cursor.fetchall()
+                inning_count = len(player_data)
+                if inning_count > 5:
+                    df = pd.DataFrame(player_data)
+                    not_out_count = len(df[df[0] == "not out"])
+                    sum_of_runs = df[1].sum()
+                    average_strike_rate = df[6].mean()
+                    if inning_count != not_out_count:
+                        average_score = sum_of_runs / (inning_count - not_out_count)
+                    else:
+                        average_score = sum_of_runs
+
+                    centuries = len(df[df[1] >= 100])
+                    fifties = len(df[df[1] >= 50]) - centuries
+                    highest_score = df[1].max()
+
+                    venue_performance = 0.4262 * average_score + 0.2566 * inning_count + 0.1510 * average_strike_rate + 0.0787 * centuries + 0.0556 * fifties + 0.0328 * highest_score
+                    db_cursor.execute(f'INSERT INTO player_venue_data_fmt (player_id, venue_id, format_id, batting_venue, bowling_venue) VALUES ({player[0]}, {venue[0]}, {format[0]}, {venue_performance}, 0) ON CONFLICT (player_id, venue_id, format_id) DO UPDATE SET batting_venue = {venue_performance}')
+
+                # Bowling venue
+                db_cursor.execute(
+                    f'SELECT bw.balls, bw.runs, bw.wickets, bw.econ, md.venue_id, md.format_id FROM bowling_data bw LEFT JOIN match_details md ON bw.match_id = md.match_id WHERE bw.player_id = {player[0]} AND md.venue_id = {venue[0]} AND md.format_id = {format[0]}'
+                )
+                player_data = db_cursor.fetchall()
+                inning_count = len(player_data)
+                if inning_count > 5:
+                    df = pd.DataFrame(player_data)
+                    total_overs = df[0].sum() / 6
+                    strike_rate = df[0].sum() / df[2].sum()
+                    average = df[1].sum() / df[2].sum()
+                    ff = len(df[df[2] >= 5])
+
+                    venue_performance = 0.4174 * total_overs + 0.2634 * inning_count + 0.1602 * strike_rate + 0.0975 * average + 0.0615 * ff
+                    db_cursor.execute(f'UPDATE player_venue_data_fmt SET bowling_venue = {venue_performance} WHERE player_id = {player[0]} AND venue_id = {venue[0]} AND format_id = {format[0]}')
+
+    for opposition in opposition_list:
+        for format in format_list:
+            for player in players_list:
+                # Batting opposition
+                db_cursor.execute(
+                    f'SELECT bd.description, bd.runs, bd.balls, bd.minutes, bd.fours, bd.sixes, bd.strike_rate, md.opposition_id, md.format_id FROM batting_data bd LEFT JOIN match_details md ON bd.match_id = md.match_id WHERE bd.player_id = {player[0]} AND md.opposition_id = {opposition[0]} AND md.format_id = {format[0]}'
+                )
+                player_data = db_cursor.fetchall()
+                inning_count = len(player_data)
+                if inning_count > 5:
+                    df = pd.DataFrame(player_data)
+                    not_out_count = len(df[df[0] == "not out"])
+                    sum_of_runs = df[1].sum()
+                    average_strike_rate = df[6].mean()
+                    if inning_count != not_out_count:
+                        average_score = sum_of_runs / (inning_count - not_out_count)
+                    else:
+                        average_score = sum_of_runs
+
+                    centuries = len(df[df[1] >= 100])
+                    fifties = len(df[df[1] >= 50]) - centuries
+                    highest_score = df[1].max()
+
+                    opposition_performance = 0.4262 * average_score + 0.2566 * inning_count + 0.1510 * average_strike_rate + 0.0787 * centuries + 0.0556 * fifties + 0.0328 * highest_score
+                    db_cursor.execute(f'INSERT INTO player_opposition_data_fmt (player_id, opposition_id, format_id, batting_opposition, bowling_opposition) VALUES ({player[0]}, {opposition[0]}, {format[0]}, {opposition_performance}, 0) ON CONFLICT (player_id, opposition_id, format_id) DO UPDATE SET batting_opposition = {opposition_performance}')
+
+                # Bowling opposition
+                db_cursor.execute(
+                    f'SELECT bw.balls, bw.runs, bw.wickets, bw.econ, md.opposition_id, md.format_id FROM bowling_data bw LEFT JOIN match_details md ON bw.match_id = md.match_id WHERE bw.player_id = {player[0]} AND md.opposition_id = {opposition[0]} AND md.format_id = {format[0]}'
+                )
+                player_data = db_cursor.fetchall()
+                inning_count = len(player_data)
+                if inning_count > 5:
+                    df = pd.DataFrame(player_data)
+                    total_overs = df[0].sum() / 6
+                    strike_rate = df[0].sum() / df[2].sum()
+                    average = df[1].sum() / df[2].sum()
+                    ff = len(df[df[2] >= 5])
+
+                    opposition_performance = 0.4174 * total_overs + 0.2634 * inning_count + 0.1602 * strike_rate + 0.0975 * average + 0.0615 * ff
+                    db_cursor.execute(f'UPDATE player_opposition_data_fmt SET bowling_opposition = {opposition_performance} WHERE player_id = {player[0]} AND opposition_id = {opposition[0]} AND format_id = {format[0]}')
 
     # Batting consistency
     for player in players_list:
         db_cursor.execute(
-            f"SELECT description, runs, balls, minutes, fours, sixes, strike_rate FROM batting_data where player_id={player[0]}"
-        )
+            f'SELECT description, runs, balls, minutes, fours, sixes, strike_rate FROM batting_data where player_id={player[0]}')
         player_data = db_cursor.fetchall()
         inning_count = len(player_data)
         if inning_count > 10:
@@ -94,24 +158,13 @@ def calculate_features(db_connection):
             fifties = len(df[df[1] >= 50]) - centuries
             zeros = len(df[df[1] == 0])
 
-            consistency = (
-                0.4262 * average_score
-                + 0.2566 * inning_count
-                + 0.1510 * average_strike_rate
-                + 0.0787 * centuries
-                + 0.0556 * fifties
-                - 0.0328 * zeros
-            )
-            print(f"Batting consistency for {player[1]}: {consistency}")
-            db_cursor.execute(
-                f"INSERT INTO player_consistency_data_fmt (player_id, season_id, format_id, batting_consistency, bowling_consistency) VALUES ({player[0]}, {season[0]}, {format[0]}, {consistency}, 0) ON CONFLICT (player_id, season_id, format_id) DO UPDATE SET batting_consistency = {consistency}"
-            )
+            consistency = 0.4262 * average_score + 0.2566 * inning_count + 0.1510 * average_strike_rate + 0.0787 * centuries + 0.0556 * fifties - 0.0328 * zeros
+            db_cursor.execute(f'INSERT INTO player_consistency_data_fmt (player_id, season_id, format_id, batting_consistency, bowling_consistency) VALUES ({player[0]}, {season_list[0][0]}, {format_list[0][0]}, {consistency}, 0) ON CONFLICT (player_id, season_id, format_id) DO UPDATE SET batting_consistency = {consistency}')
 
     # Bowling consistency
     for player in players_list:
         db_cursor.execute(
-            f"SELECT balls, runs, wickets, econ FROM bowling_data where player_id={player[0]}"
-        )
+            f'SELECT balls, runs, wickets, econ FROM bowling_data where player_id={player[0]}')
         player_data = db_cursor.fetchall()
         inning_count = len(player_data)
         if inning_count > 10:
@@ -121,17 +174,8 @@ def calculate_features(db_connection):
             average = df[1].sum() / df[2].sum()
             ff = len(df[df[2] >= 5])
 
-            consistency = (
-                0.4174 * total_overs
-                + 0.2634 * inning_count
-                + 0.1602 * strike_rate
-                + 0.0975 * average
-                + 0.0615 * ff
-            )
-            print(f"Bowling consistency for {player[1]}: {consistency}")
-            db_cursor.execute(
-                f"UPDATE player_consistency_data_fmt SET bowling_consistency = {consistency} WHERE player_id = {player[0]} AND season_id = {season[0]} AND format_id = {format[0]}"
-            )
+            consistency = 0.4174 * total_overs + 0.2634 * inning_count + 0.1602 * strike_rate + 0.0975 * average + 0.0615 * ff
+            db_cursor.execute(f'UPDATE player_consistency_data_fmt SET bowling_consistency = {consistency} WHERE player_id = {player[0]} AND season_id = {season_list[0][0]} AND format_id = {format_list[0][0]}')
 
     db_connection.commit()
 
