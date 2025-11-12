56c51df292ec2053d089a5af405640db3f3eba32
umayangag <umayangag@gmail.com>
2025-11-13 00:35:22 +0530

    fix lint

--- go-app/internal/cli/teamselect/options_env_test.go
+++ go-app/internal/cli/teamselect/options_env_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestParseArgs_EnvDefaults(t *testing.T) {
-	t.Parallel()
 	t.Setenv("TEAM_SELECT_MATCH", "42")
 	t.Setenv("TEAM_SELECT_FORMAT", "t20i")
 	t.Setenv("TEAM_SELECT_SEASON", "2020")
