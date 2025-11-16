e4053792814ca3dec6b646ad83a7588cec6a3999
umayangag <umayangag@gmail.com>
2025-11-16 16:42:45 +0530

    update docs

--- go-app/cmd/cricsheet-importer/main.go
+++ go-app/cmd/cricsheet-importer/main.go
@@ -27,7 +27,7 @@ func run() int {
 
 	logger.SetupFromEnv()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Minute)
 	defer cancel()
 	if _, err := db.Connect(ctx); err != nil {
 		slog.Error("db connect failed", slog.Any("err", err))
