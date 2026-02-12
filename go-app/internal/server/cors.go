287bf4c76c6609749f9ecf77c569a7fb6567a264
umayanga <umayangag@gmail.com>
2026-02-12 05:28:07 +0100

    add cors

--- go-app/internal/server/cors.go
+++ go-app/internal/server/cors.go
@@ -17,7 +17,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, X-API-Key")
 
 		if r.Method == http.MethodOptions {
 			// Preflight response
